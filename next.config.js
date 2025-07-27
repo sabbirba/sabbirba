be625ee1d9a465f430eec2158c89a76fa1631a09
Sabbir Bin Abbas <sabbirba10@gmail.com>
2025-07-28 03:36:45 +0600

    chore: Comment out output export option in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { withSentryConfig } = require("@sentry/nextjs");
 
 const nextConfig = {
-  output: 'export', 
+  // output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
