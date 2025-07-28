1fce88b1420a10ecbc654a55c7d400bdfce2b314
Sabbir Bin Abbas <sabbirba10@gmail.com>
2025-07-28 13:30:36 +0600

    chore: Remove Sentry configuration and image remote patterns from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
-const { withSentryConfig } = require("@sentry/nextjs");
 
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "**.githubusercontent.com" },
-      { protocol: "https", hostname: "**.github.com" },
-      { protocol: "https", hostname: "**.amazonaws.com" },
-    ],
-  },
 };
 
-// module.exports = withSentryConfig(nextConfig, { silent: false });
 module.exports = nextConfig;
